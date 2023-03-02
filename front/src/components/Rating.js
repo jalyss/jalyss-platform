3d8e368c2ae38ae42b8e600b087cd22b972270eb
GHADA KAMMOUN <104655932+ghada3ER@users.noreply.github.com>
2023-03-02 21:29:29 +0100

    article card

---
+++ front/src/components/Rating.js
@@ -0,0 +1,17 @@
+import { AiFillStar } from 'react-icons/ai'
+
+function Rating({ rate, onChangeRate, disabled }) {
+  const stars = ['1', '2', '3', '4', '5']
+
+  return (
+    <div>
+      {stars.map((star) => (
+        <span key={star} onClick={() => !disabled && onChangeRate(star)}>
+          <AiFillStar fill={star <= rate ? '#edcc3f' : '#d4d4d4'} />
+        </span>
+      ))}
+    </div>
+  )
+}
+
+export default Rating
