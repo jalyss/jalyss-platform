128756493458e54594912464a40b82c24b042933
unknown <brkhayreddine@gmail.com>
2023-05-20 21:48:05 +0100

    last modif

--- back-office/src/router/Router.js
+++ back-office/src/router/Router.js
@@ -22,7 +22,7 @@ import ArticleList from "../domains/articles/views/ArticleList";
 import CreateArticle from "../domains/articles/views/CreateArticle";
 import ArticleByBranchList from "../domains/articles/views/ArticleByBranchList";
 import Dashboard from "../domains/dashboard/Dashboard";
-import Charts from "../domains/charts/Charts";
+// import Charts from "../domains/charts/Charts";
 import Command from "../domains/commands/Command";
 import CommandList from "../domains/commands/views/CommandList";
 import CreateCommand from "../domains/commands/views/CreateCommand";
--- front/src/pages/BlogsForm.js
+++ front/src/pages/BlogsForm.js
@@ -1,40 +1,67 @@
-
 import React, { useState } from "react";
-import { Button, Typography,Form } from "antd";
+import { Button, Typography, Form } from "antd";
 import QuillEditor from "../components/QuillEditor";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
 const { Title } = Typography;
 
 const BlogsForm = () => {
-
   const [content, setContent] = useState("");
-  const [files,setFiles]=useState([])
+  const [files, setFiles] = useState([]);
 
-  
-  const onEditorChange = (newContent ) => {
+  const onEditorChange = (newContent) => {
     setContent(newContent);
-    localStorage.setItem('blogContent', newContent); // store the content in localStorage
-    console.log('newContent',newContent);
-  };
-  const onFilesChange = (files ) => {
-    setFiles(files);
-    console.log('files',files);
+    localStorage.setItem("blogContent", newContent);
+    console.log("newContent", newContent);
   };
 
- 
-  // Retrieve the content from localStorage on component mount
+  const onFilesChange = (files) => {
+    setFiles(files);
+    console.log("files", files);
+  };
+
   useState(() => {
-    const storedContent = localStorage.getItem('blogContent');
+    const storedContent = localStorage.getItem("blogContent");
     if (storedContent) {
       setContent(storedContent);
     }
   }, []);
 
+  const decoded = jwt.verify(token, "your secret or key");  
+var userId = decoded.id  
+console.log(userId)  
 
-  
   const handleClearStorage = () => {
     localStorage.clear();
-    setContent('');
+    setContent("");
   };
+
+  const handleSubmit = async () => {
+    try {
+
+      const response = await axios.post(
+        `localhost:3000/api/v1/blogs`,
+        {body:{
+          content,
+          userId,
+          categoryId,
+        }
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("bearer"+json.parse(localstorage.getItem("token")).Authorization)
+          },
+        }
+      );
+      const data = response.data;
+console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+  
   return (
     <div>
       <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
@@ -42,30 +69,21 @@ const BlogsForm = () => {
           <Title level={2}>Editor</Title>
         </div>
         <QuillEditor
-          placeholder={"Start Posting Somthing"}
-         onEditorChange={onEditorChange}
-         onFilesChange={onFilesChange}
+          placeholder={"Start Posting Something"}
+          onEditorChange={onEditorChange}
+          onFilesChange={onFilesChange}
         />
 
-        {/* <textarea value={text} onChange={(e) => setText(e.target.value)} /> */}
-      <Form>
-        <div style={{ textAlign:"center",margin:"2rem auto"}}>
-          <Button
-          size="large"
-          htmlType="submit"
-          className=""
-
-          >
-          submit
-          </Button>
-        </div>
-      </Form>
+      
+            <Button onClick={handleSubmit} >
+              Submit
+            </Button>
+          
       </div>
       <div>
         <h2>Content from localStorage:</h2>
         <button onClick={handleClearStorage}>Clear Storage</button>
         <div dangerouslySetInnerHTML={{ __html: content }} />
-
       </div>
     </div>
   );
