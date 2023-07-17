4667d6bf6af0238334ed11b9a7e4328d799be807
khalil kraiem <khalilmedkraiem@gmail>
2023-07-17 23:53:19 +0100

    pulling front work of chat

--- back/src/domains/chat/chat.module.ts
+++ back/src/domains/chat/chat.module.ts
@@ -8,10 +8,11 @@ import { ChatGateway } from './chat.gateway';
 import { UsersService } from '../users/users.service';
 import { ConnectedUsersService } from './connectedUsers.service';
 import { ConnectedUserController } from './connectedUsers.controller';
+import { UserChatroomService } from './user-chatroom.service';
 
 @Module({
-  controllers: [ChatRoomController, MessagesController,ConnectedUserController],
+  controllers: [ChatRoomController, MessagesController,ConnectedUserController,UserChatroomService],
 
-  providers: [ChatRoomService, MessagesService, PrismaService,ChatGateway,UsersService,ConnectedUsersService],
+  providers: [ChatRoomService, MessagesService, PrismaService,ChatGateway,UsersService,ConnectedUsersService,UserChatroomService],
 })
 export class ChatModule {}
