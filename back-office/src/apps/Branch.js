ad876ae02bc4b336881048f73d19b6f1ea9ec6e9
khalil kraiem <khalilmedkraiem@gmail>
2023-07-17 12:53:29 +0100

    mergin confict of sidebar work

--- back-office/src/apps/Branch.js
+++ back-office/src/apps/Branch.js
@@ -1,31 +1,25 @@
-import React, { useMemo, useState } from 'react';
-import Sidebar from '../layouts/Sidebar';
-import Header from '../layouts/Header';
-import { Outlet } from 'react-router-dom';
-import { Box } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import React, { useMemo } from 'react'
+import Sidebar from '../layouts/Sidebar'
+import Header from '../layouts/Header'
+import { Outlet } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 
 function Branch() {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
-
-  const handleSidebarToggle = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
-  const { i18n } = useTranslation();
-  const isRtl = useMemo(() => i18n?.languages[0] === 'ar', [i18n?.languages]);
+  const { i18n } = useTranslation()
+  const isRtl = useMemo(() => i18n?.languages[0] === 'ar', [i18n?.languages])
 
   return (
     <div>
-      <div className="d-flex">
-        {!isSidebarOpen && <Sidebar />}
+      <div className={`d-flex `}>
+        <Sidebar />
         <div className="w-100">
-          <Header isSidebarClosed={isSidebarOpen} handleSidebarToggle={handleSidebarToggle} />
+          <Header />
           <Box
-            width={`calc(100% - ${isSidebarOpen ? '260px' : '0px'})`}
+            width="calc(100% - 260px)"
             mr={isRtl && '260px'}
             ml={!isRtl && '260px'}
-            mt={isSidebarOpen && '80px'}
+            mt='80px'
             className="pages"
           >
             <Outlet />
@@ -33,7 +27,7 @@ function Branch() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 
