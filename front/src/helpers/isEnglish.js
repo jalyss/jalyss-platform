4fad4258d98db9516128a349283911ef89bf99d2
khalil kraiem <khalilmedkraiem@gmail>
2023-07-20 14:57:40 +0100

    navbar and routing for production

--- front/src/helpers/isEnglish.js
+++ front/src/helpers/isEnglish.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import useLanguage from '../hooks/useLanguage'
 
 function isEnglish() {
--- front/src/router/Router.js
+++ front/src/router/Router.js
@@ -38,12 +38,10 @@ import OrderHistory from "../components/Profile/OrderHistory";
 
 import Chat from "../pages/Chat";
 
-
 import SessionDetails from "../pages/SessionDetails";
 
-import ReserveMeeting from "../pages/space/SpaceReservation"
+import ReserveMeeting from "../pages/space/SpaceReservation";
 import Conversation from "../components/chatComponents/Conversation";
-
 
 function Router() {
   return (
@@ -52,11 +50,12 @@ function Router() {
         <Routes>
           <Route path="/" element={<Client />}>
             <Route index element={<Home />} />
-            <Route path="articles" element={<Articles />} />
+            <Route path="articles" element={<NoPage />} />
+            {/*<Route path="articles" element={<Articles />} />
             <Route path="articles/cat/:categoryId" element={<Articles />} />
             <Route path="one-article/:articleId" element={<OneArticle />} />
             <Route path="checkout" element={<Checkout />} />
-            <Route path="invoice/:invoiceId" element={<Invoice />} />
+            <Route path="invoice/:invoiceId" element={<Invoice />} /> */}
             <Route path="profile" element={<Profile />}>
               <Route index element={<MyBlogs />} />
               <Route path="my-bookmarks" element={<MyBookmarks />} />
@@ -69,33 +68,29 @@ function Router() {
             <Route path="signup" element={<Signup />} />
             <Route path="reset-password" element={<ResetPassword />} />
             <Route path="new-password" element={<NewPassword />} />
-            
+
             <Route path="*" element={<NoPage />} />
             <Route path="/chat" element={<Chat />}>
-            <Route path="/chat/:userId" element={<Conversation />} />
-           
-
+              <Route path="/chat/:userId" element={<Conversation />} />
             </Route>
             <Route path="training" element={<TrainingPage />} />
             <Route path="mentor" element={<MentorPage />} />
+            <Route path="sessions/:sessionId" element={<SessionDetails />} />
+
             <Route path="blogs" element={<Blogs />} />
             <Route path="blogs/:blogId" element={<BlogsDetail />} />
             <Route path="blogsForm" element={<BlogsForm />} />
-            <Route path="blogs/:blogId" element={<UpdateBlog />} />
             <Route path="update-blog/:blogId" element={<UpdateBlog />} />
-            <Route path="spaceJalyss" element={<SpaceJalyss />} />
+
+            <Route path="spaceJalyss" element={<NoPage />} />
+            {/* <Route path="spaceJalyss" element={<SpaceJalyss />} />
             <Route
               path="spaceJalyss/:serviceIdentifier"
               element={<ServiceSpace />}
             />
-           
 
-
-            <Route path="update-blog/:blogId" element={<UpdateBlog />} />
-            <Route path="sessions/:sessionId" element={<SessionDetails />}/>
-           
             <Route path="RegisterForm" element={<RegisterForm />} />
-            <Route  path="SpaceReservation" element={ <SpaceReservation/>}/>
+            <Route path="SpaceReservation" element={<SpaceReservation />} /> */}
           </Route>
         </Routes>
       </BrowserRouter>
@@ -104,4 +99,3 @@ function Router() {
 }
 
 export default Router;
-
