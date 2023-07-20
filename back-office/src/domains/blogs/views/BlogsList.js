1d1ae39c2f61231b3cc771734a6b773db68d3632
omar massoudi <massoudiomar54321@gmail.com>
2023-07-20 13:39:03 +0100

    defred

--- back-office/src/domains/blogs/views/BlogsList.js
+++ back-office/src/domains/blogs/views/BlogsList.js
@@ -1,17 +1,15 @@
-import { GridActionsCellItem } from "@mui/x-data-grid";
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AiFillDelete, AiOutlineEye } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
-import Box from "@mui/material/Box";
-import { DataGrid } from "@mui/x-data-grid";
-import { showErrorToast, showSuccessToast } from "../../../utils/toast";
-import isEnglish from "../../../helpers/isEnglish";
-import { Dropdown, DropdownButton } from "react-bootstrap";
-import { rows } from "../../../constants/blogData";
-import { fetchBlogs } from "../../../store/blogs";
-import Modal from "../../../components/Commun/Modal";
-import DropDown from "../../../components/Commun/DropDown";
+import { GridActionsCellItem } from '@mui/x-data-grid';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AiFillDelete, AiOutlineEye } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { showErrorToast, showSuccessToast } from '../../../utils/toast';
+import isEnglish from '../../../helpers/isEnglish';
+import { fetchBlogs } from '../../../store/blogs';
+import Modal from '../../../components/Commun/Modal';
+import AutoCompleteFilter from '../../../components/Commun/AutoCompleteFilter';
 
 function BlogsList() {
   const [show, setShow] = useState(false);
@@ -20,27 +18,42 @@ function BlogsList() {
   const take = 50;
   const trend = 0;
   const [basicModal, setBasicModal] = useState(false);
-
+  const [selectedSituation, setSelectedSituation] = useState(null);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const dispatch = useDispatch();
-  const colorReference = "#48184c";
+  const colorReference = '#48184c';
+  const situations = [
+    { value: 'Confirmed', label: 'Confirmed' },
+    { value: 'Refused', label: 'Refused' },
+    { value: 'Pending', label: 'Pending' },
+  ];
 
   const blogs = useSelector((state) => state.blogs.blogs.items);
-  console.log(blogs, "omar");
-  //   const blogss = blogs?.map((elem) => ({
-  //     id: elem.id,
-  //     content: elem.content,
-  //   }));
-  const rows = blogs?.map((elem) => ({
-    id: elem.id,
-    name: elem.author.fullNameEn,
-    blogTitle: elem.title,
-    articleCategory: elem.category.nameEn,
-    date: elem.createdAt,
-    content: elem.content,
-    situation: elem.confirm,
-  }));
+  function getDataByConfirmStatus(dataArray, confirmStatus) {
+    return dataArray.filter((item) => item.confirm === confirmStatus);
+  }
+  const confirmedPosts = getDataByConfirmStatus(blogs, !true); 
+    const rows = confirmedPosts
+    ?.map((elem) => ({
+      id: elem.id,
+      name: elem.author.fullNameEn,
+      blogTitle: elem.title,
+      articleCategory: elem.category.nameEn,
+      date: elem.createdAt,
+      content: elem.content,
+      situation: elem.confirm,
+      reason: elem.reason,
+    }))
+    .filter((elem) => {
+      if (!selectedSituation) {
+        // If no situation is selected, show all data
+        return true;
+      } else {
+        // Filter the data based on the selected situation
+        return elem.situation.toLowerCase() === selectedSituation.toLowerCase();
+      }
+    });
 
   useEffect(() => {
     dispatch(fetchBlogs({ take, skip }));
@@ -48,40 +61,40 @@ function BlogsList() {
 
   const buttonStyle = {
     backgroundColor: colorReference,
-    color: "white",
-    borderRadius: "5px",
+    color: 'white',
+    borderRadius: '5px',
   };
   const toggleShow = () => {
     setBasicModal(!basicModal);
   };
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
-    { field: "name", headerName: "Name", width: 150, editable: false },
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 150, editable: false },
     {
-      field: "blogTitle",
-      headerName: "Blog Title",
+      field: 'blogTitle',
+      headerName: 'Blog Title',
       width: 150,
       editable: false,
     },
     {
-      field: "articleCategory",
-      headerName: "Category",
+      field: 'articleCategory',
+      headerName: 'Category',
       width: 150,
       editable: false,
     },
-    { field: "date", headerName: "Date", width: 150, editable: false },
+    { field: 'date', headerName: 'Date', width: 150, editable: false },
     {
-      field: "situation",
-      headerName: "Situation",
+      field: 'situation',
+      headerName: 'Situation',
       width: 150,
       editable: false,
     },
     {
-      field: "actions",
-      type: "actions",
-      headerName: "Actions",
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
       width: 150,
-      cellClassName: "actions",
+      cellClassName: 'actions',
       getActions: ({ id }) => {
         return [
           <GridActionsCellItem
@@ -91,7 +104,6 @@ function BlogsList() {
             onClick={() => handleAddClick(id)}
             color="success"
           />,
-
           <GridActionsCellItem
             icon={<AiFillDelete />}
             label="Delete"
@@ -107,8 +119,8 @@ function BlogsList() {
   const isEng = isEnglish();
   const Navigate = useNavigate();
   // const handleDeleteClick = (id) => {
-
   // };
+
   const handleAddClick = (blogId) => {
     Navigate(`detail/${blogId}`);
   };
@@ -116,22 +128,18 @@ function BlogsList() {
   return (
     <div>
       <div className="container">
-        <h2 style={{ paddingLeft: 10, paddingTop: 10 }}>
-          List of people who create blogs
-        </h2>
+        <h2 style={{ paddingLeft: 10, paddingTop: 10 }}>List of people who create blogs</h2>
         <hr></hr>
-        <DropDown
-          content1={"Accept"}
-          content2={"Refuse"}
-          content3={"Waiting"}
-        />
-        {/* <DropdownButton style={buttonStyle} id="dropdownMenuButton" className='mb-3' title="Choose a situation ">
-        <Dropdown.Item href="#">Accept</Dropdown.Item>
-        <Dropdown.Item href="#">Refuse</Dropdown.Item>
-        <Dropdown.Item href="#">Waiting</Dropdown.Item>
-      </DropdownButton> */}
 
-        <Box sx={{ height: 400, width: "100%" }}>
+        <AutoCompleteFilter
+          data={situations}
+          valueOptionName="value"
+          labelOptionName="label"
+          label="Filter by situation"
+          onChange={(selectedValue) => setSelectedSituation(selectedValue?.value)}
+        />
+
+        <Box sx={{ height: 400, width: '100%' }}>
           <DataGrid
             rows={rows}
             columns={columns}
@@ -146,14 +154,10 @@ function BlogsList() {
             disableRowSelectionOnClick
           />
         </Box>
-        <Modal
-          bodOfDelete={"are"}
-          basicModal={basicModal}
-          toggleShow={toggleShow}
-          ofDelete={true}
-        />
+        <Modal bodOfDelete={'are'} basicModal={basicModal} toggleShow={toggleShow} ofDelete={true} />
       </div>
     </div>
   );
 }
+
 export default BlogsList;
