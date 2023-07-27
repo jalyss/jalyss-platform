4eead8dd780ca66bedcc2f1fd5072b81146bf1cb
raniaelouni <raniaelouni421@gmail.com>
2023-07-27 19:36:04 +0100

    trainingBooking backend

--- back/src/domains/training/trainingBooking.service.ts
+++ back/src/domains/training/trainingBooking.service.ts
@@ -1,48 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTrainingBookingDto } from './dto/create-TrainingBooking.dto';
 import { UpdateTrainingBookingDto } from './dto/update-TrainingBooking.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { session } from 'passport';
 
 @Injectable()
-export class TrainingBookingService  {
-  constructor(private readonly prisma:PrismaService) {}
-   async create(dto: CreateTrainingBookingDto,userId:string) {
+export class TrainingBookingService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(dto: CreateTrainingBookingDto, userId: string) {
+   const session= await this.prisma.session.findFirst({where:{tarifs:{some:{id:dto.sessionTarifId}}}})
+   console.log('session=======>',session);
+   
+   const tarifBookings =await this.prisma.trainingBooking.findMany({where:{
+      sessiontarif:{
+        sessionId:session.id
+      },
+      userId
+    }})
+    console.log('tarifBookings=======>',tarifBookings);
+    if(tarifBookings.length)
+    throw new HttpException("already booked",HttpStatus.BAD_REQUEST)
     return await this.prisma.trainingBooking.create({
-      data : {
+      data: {
         ...dto,
-        userId
-      }
+        userId,
+      },
     });
   }
 
-   async findAll() {
-    return await this.prisma.trainingBooking.findMany({ 
-      include : {
-        user : true, 
-        sessiontarif : true, 
-      }
-       });
+  async findAll() {
+    return await this.prisma.trainingBooking.findMany({
+      include: {
+        user: true,
+        sessiontarif: { include: { session: true } },
+      },
+    });
   }
 
-  async findOne(id:string) {
-    return await this.prisma.trainingBooking.findUnique(
-      {
-        where : {id}
-      }
-    );;
+  async findOne(id: string) {
+    return await this.prisma.trainingBooking.findUnique({
+      where: { id },
+    });
   }
 
-  async update(id:string, dto: UpdateTrainingBookingDto) {
+  async update(id: string, dto: UpdateTrainingBookingDto) {
     return await this.prisma.trainingBooking.update({
-      where : {id}, 
-      data : dto,
-    })
+      where: { id },
+      data: dto,
+    });
   }
 
-  async remove(id:string) {
+  async remove(id: string) {
     return await this.prisma.trainingBooking.delete({
-      where : {id},
-    })
+      where: { id },
+    });
   }
 }
