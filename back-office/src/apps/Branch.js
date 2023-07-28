1269db499ff7d6696ebecc7b482cf116ecf4141b
khalil kraiem <khalilmedkraiem@gmail>
2023-07-28 15:46:23 +0100

    @mui/x-date-pickers back-office

--- back-office/src/apps/Branch.js
+++ back-office/src/apps/Branch.js
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { socket } from "../utils/socketIo";
+import { socket } from "../utils/SocketIo";
 
 export const SocketContext = createContext();
 
