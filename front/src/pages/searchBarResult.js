57fe83b88fdefbc7cf9b4e4eb476ad85b4e4ad1b
jdidi94 <jdididaoud1994@gmail.com>
2023-08-22 23:28:03 +0100

    adding the search engine

--- front/src/pages/searchBarResult.js
+++ front/src/pages/searchBarResult.js
@@ -2,14 +2,23 @@ import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchArticles } from "../store/article";
+import funcs from "../helpers/searchFunctions";
 const SearchbarResult = () => {
   const location = useLocation();
   const dispatch = useDispatch();
   const aricleStore = useSelector((state) => state.article);
-  console.log(aricleStore);
+  console.log("check", aricleStore);
+  const filterArticles = () => {
+    let arr = aricleStore.articles.items.filter((article) => {
+      return funcs.checkValueInObject(article, location.state.message);
+    });
+    console.log("qcxvbnhjkl", arr);
+  };
   useEffect(() => {
+    console.log("location", location.state);
     dispatch(fetchArticles());
-  }, []);
+    filterArticles();
+  }, [location.state]);
   // const sessionsStore = useSelector((state) => state.auth);
   // const spaceStore = useSelector((state) => state.auth);
   // const blogStore=useSelector((state) => state.auth);
