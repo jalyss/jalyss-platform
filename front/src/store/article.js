9d7b28a9ec82d820a05a9d300791aaee95626bd1
jdidi94 <jdididaoud1994@gmail.com>
2023-08-29 21:40:35 +0100

    done

--- front/src/store/article.js
+++ front/src/store/article.js
@@ -25,12 +25,29 @@ export const fetchArticlesByBranch = createAsyncThunk(
   }
 );
 
-export const fetchArticle = createAsyncThunk("articles/article", async (id) => {
+export const findByCategory = createAsyncThunk("articles", async (args) => {
+  const identifier = args.identifier;
+  delete args.identifier;
+  console.log(args, "args");
   const response = await axios.get(
-    `${config.API_ENDPOINT}/articles/one-by-branch/${id}`
+    `${config.API_ENDPOINT}/findByCategory/${identifier ? identifier : null}`
   );
   return response.data;
 });
+
+export const fetchArticle = createAsyncThunk("articles/article", async (id) => {
+  const response = await axios.get(`${config.API_ENDPOINT}/articles/one/${id}`);
+  return response.data;
+});
+export const fetchArticleByBranch = createAsyncThunk(
+  "articles/articleByBranch",
+  async (id) => {
+    const response = await axios.get(
+      `${config.API_ENDPOINT}/articles/one-by-branch/${id}`
+    );
+    return response.data;
+  }
+);
 
 export const createArticleByBranchRating = createAsyncThunk(
   "articles/rating",
@@ -78,6 +95,9 @@ export const articleSlice = createSlice({
     builder.addCase(fetchArticlesByBranch.fulfilled, (state, action) => {
       state.articles.items = action.payload;
     });
+    builder.addCase(findByCategory.fulfilled, (state, action) => {
+      state.articles.items = action.payload;
+    });
     builder.addCase(fetchArticle.fulfilled, (state, action) => {
       state.article = action.payload;
     });
@@ -86,4 +106,5 @@ export const articleSlice = createSlice({
     });
   },
 });
+
 export default articleSlice.reducer;
