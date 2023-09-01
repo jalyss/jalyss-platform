c1da2899e38dd319e234183fbecdc34ae7bec272
ihebKedri <ihebkedri02@gmail.com>
2023-09-01 11:43:24 +0100

    wip

--- front/src/components/Profile/SpaceBooked.js
+++ front/src/components/Profile/SpaceBooked.js
@@ -31,38 +31,58 @@ const SpaceBooked = () => {
     <div className="d-flex flex-wrap justify-content-center align-items-center" style={{ maxWidth: "700px", maxHeight: "600px" }}>
       { data?.map((el) => (
      
-         <div className="card"   style={{ height: "200px", maxWidth: "300px", margin: "10px" }}>
+         <div className="card"   style={{ maxheight: "250px", maxWidth: "300px"}}  >
           <img
+         
             className="card-img-top"
             src="https://images.pexels.com/photos/3184669/pexels-photo-3184669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             alt="Card image"
           /> 
-            <div className="card-body ">
-            <h5 className="card-title" style={{ color: 'purple' }}>Company :</h5>
-            <h6 className="card-title">{el.companyName}</h6>
-            <h5 className="card-title" style={{ color: 'purple' }}>Tarif Description :</h5>
-            <h6 className="card-title" >{el?.tarif?.description}</h6>
-            <h5 className="card-title" style={{ color: 'purple' }}>Free Space :</h5>
-            <h6 className="card-title">{el.freeSpace}</h6>
-            <ul className="list-group list-group-flush">
-            <li className="list-group-item">Start Time : {el.startTime}</li>
+  <div className="card-body ">
+
+  
+      <h5 className="card-title" style={{ color: 'purple', marginBottom: 0 }}>Company:</h5>
+      <h6 className="card-title">{el.companyName}</h6>
     
-              <li className="list-group-item">End Time : {el.endTime}</li>
-              <li className="list-group-item">Price : {el.tarif.price}</li>
-              <li className="list-group-item">
-                {el.paid ? (
-                  <i className="fas fa-check-circle text-success"></i>
-                ) : (
-                  <i className="fas fa-times-circle text-danger"></i>
-                )}
-                {el.paid ? " Paid" : " Not Paid"}
-              </li>
-            </ul>
-            
-          </div>
+
+      <h5 className="card-title" style={{ color: 'purple', marginBottom: 0 }}>Tarif Description:</h5>
+      <h6 className="card-title">{el?.tarif?.description}</h6>
+  
+  
+  
+      <h5 className="card-title" style={{ color: 'purple', marginBottom: 0 }}>Free Space:</h5>
+      <h6 className="card-title">{el.freeSpace}</h6>
+  
+      </div>
+
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">
+    <div className="d-flex justify-content-between align-items-center">
+    <span>Start Time: {el.startTime}</span>
+    
+    <span>End Time: {el.endTime}</span> 
+    </div>
+    </li>
+    <li className="list-group-item">
+      <div className="d-flex justify-content-between align-items-center">
+        <span>Price: {el?.tarif?.price}DT</span>
+        
+        <span>
+          {el.paid ? (
+            <i className="fas fa-check-circle text-success"></i>
+          ) : (
+            <i className="fas fa-times-circle text-danger"></i>
+          )}
+          {el.paid ? "Paid" : "Not Paid"}
+        </span>
+      </div>
+    </li>
+  </ul>
+</div>
+
+
           
-      
-        </div>
+
          ))}
     </div>
   );
