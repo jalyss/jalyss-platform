188d37d502921b1effd977a407b5322823307d96
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-09-14 17:05:11 +0100

    Blog

--- front/src/pages/Blogs.js
+++ front/src/pages/Blogs.js
@@ -25,7 +25,7 @@ import { showErrorToast, showSuccessToast } from "../utils/toast";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUsers } from "../store/user";
 import { createBookmark } from "../store/bookmarks";
-import { fetchBlogs, fetchTrends,removeBlog } from "../store/blog";
+import { fetchBlogs, fetchTrends, removeBlog } from "../store/blog";
 
 
 
@@ -49,7 +49,7 @@ function Blogs() {
   const [basicModal, setBasicModal] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
 
-
+  console.log(me.client.isCoach, "ttttttt")
   const take = 6;
 
   const greeting = {
@@ -95,8 +95,8 @@ function Blogs() {
     });
   };
 
- 
-  return  (
+
+  return (
     <DocumentMeta {...meta} className="container-fluid">
       <div>
         <Fade bottom duration={1000} distance="40px">
@@ -137,8 +137,25 @@ function Blogs() {
                         Explore &#9654;
                       </div>
                     </button>
+                    {me?.client.isCoach && (
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger"
+                        style={{
+                          height: "50px",
+                          width: "172px",
+                          marginLeft: "20px",
+                        }}
+                        onClick={() => {
+                          navigate("/BlogsForm");
+                        }}
+                      >
+                        Write yours
+                      </button>
+                    )}
 
-                    <button
+                    {/* <button
+
                       type="button"
                       className="btn btn-outline-danger"
                       style={{
@@ -147,12 +164,15 @@ function Blogs() {
                         marginLeft: "20px",
                       }}
                       onClick={() => {
-                        if (me) navigate("/BlogsForm");
+                        if (me?.client.isCoach) navigate("/BlogsForm");
                         else navigate("/login");
-                      }}
+                      }
+                      
+                    }
+                    disabled={!me?.client.isCoach} 
                     >
                       Write yours
-                    </button>
+                    </button> */}
                   </div>
                 </div>
               </div>
@@ -182,7 +202,7 @@ function Blogs() {
                         className="text-center"
                         style={{ marginBottom: "20px", fontSize: "50px" }}
                       >
-                        Trending on Jalyss <br /> <FaFire />
+                        Best Author Blog <br /> <FaFire />
                       </h1>
                       <div className="d-flex flex-wrap justify-content-center">
                         {trends.map((blog, index) => (
@@ -337,7 +357,7 @@ function Blogs() {
                   </div>
                 </div>
 
-                
+
                 <Dropdown>
                   <Dropdown.Toggle
                     className="ellipsis-btn dropdownToggleBlogCard"
@@ -356,7 +376,7 @@ function Blogs() {
                         >
                           Delete
                         </Dropdown.Item>
-                        <Dropdown.Item  onClick={() => navigate(`/update-blog/${blog.id}`)}>Update</Dropdown.Item>
+                        <Dropdown.Item onClick={() => navigate(`/update-blog/${blog.id}`)}>Update</Dropdown.Item>
                       </>
                     ) : (
                       <Dropdown.Item
