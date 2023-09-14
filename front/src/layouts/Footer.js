3e1b626ae52fb75ea5feaa6129f975be69867ae0
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-09-14 13:16:29 +0100

    whatsapp-icon-should-be-removed

--- front/src/layouts/Footer.js
+++ front/src/layouts/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import WhatsAppWidget from "react-whatsapp-chat-widget";
+
 import "react-whatsapp-chat-widget/index.css";
 
 import { MDBFooter, MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
@@ -34,38 +34,7 @@ function Footer() {
 
   return (
     <MDBFooter className="bg-darkPurple pt-3">
-      <WhatsAppWidget
-        phoneNo="+216 56 525 519"
-        position="right"
-        widgetWidth="300px"
-        widgetWidthMobile="260px"
-        autoOpen={false}
-        autoOpenTimer={5000}
-        messageBox={true}
-        messageBoxTxt="Hi Jalyss Team, is there any related service available ?"
-        iconSize="40"
-        iconColor="white"
-        iconBgColor="#25D366"
-        headerIcon="https://www.pdapps.net.in/_next/static/media/android-chrome-192x192.9a39c2c7.png"
-        headerIconColor="pink"
-        headerTxtColor="black"
-        headerBgColor="#25D366"
-        headerTitle="Jalyss Support"
-        headerCaption="Online"
-        bodyBgColor="#ECE5DD"
-        chatPersonName="Jalyss Support"
-        chatMessage={
-          <>
-            Hi there 👋 <br />
-            <br /> How can I help you?
-          </>
-        }
-        footerBgColor="#aaaaaa"
-        placeholder="Type a message.."
-        btnBgColor="#25D366"
-        btnTxt="Start Chat"
-        btnTxtColor="black"
-      />
+      
       <MDBContainer>
         <MDBRow className="text-center mb-5 justify-content-center">
           <MDBCol>
