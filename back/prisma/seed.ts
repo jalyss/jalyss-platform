7f3c0234097d9501ee95737572d91261b84d8bac
raniaelouni <raniaelouni421@gmail.com>
2023-09-15 20:11:59 +0100

    pulling

--- back/prisma/seed.ts
+++ back/prisma/seed.ts
@@ -1697,13 +1697,13 @@ async function main() {
           ],
       },
     });
-    for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
-      await prisma.view.create({
-        data: {
-          blogId: newBlog.id,
-        },
-      });
-    }
+    // for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
+    //   await prisma.view.create({
+    //     data: {
+    //       blogId: newBlog.id,
+    //     },
+    //   });
+    // }
   }
   //create session
   let lectures = [];
