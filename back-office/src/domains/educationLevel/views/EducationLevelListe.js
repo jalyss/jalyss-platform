5f9a4d19fd1d9f8cde2b1e255e0695eaf669430e
mehdi_jaouedi <mehdijaouedi9@gmaile.com>
2023-09-25 23:38:06 +0100

    WIP

--- back-office/src/domains/educationLevel/views/EducationLevelListe.js
+++ back-office/src/domains/educationLevel/views/EducationLevelListe.js
@@ -3,22 +3,35 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   fetchEducationLevels,
   removeEducationLevel,
+  createEducationLevel,
 } from "../../../store/educationLevel";
 import { showErrorToast, showSuccessToast } from "../../../utils/toast";
 import { AiFillDelete, AiOutlineEye } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
-import AddButton from "../../../components/Commun/buttons/AddButton";
 import Modal from "../../../components/Commun/Modal";
+import { GridActionsCellItem } from "@mui/x-data-grid";
 
-import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
+import {
+ 
+  Box,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
 function EducationLevelList() {
-  const [basicModal, setBasicModal] = useState(false);
   const educationLevelStore = useSelector((state) => state.educationLevel);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [rows, setRows] = useState([]);
   const [selectededucationLevelId, setSelectededucationLevelId] = useState("");
+  const [basicModal, setBasicModal] = useState(false);
+  const [nameAr, setNameAr] = useState("");
+  const [nameEn, setNameEn] = useState("");
+  const [isAddLevelModalOpen, setAddLevelModalOpen] = useState(false); // State for Add Level Modal
+
   const toggleShow = () => {
     setBasicModal(!basicModal);
   };
@@ -34,10 +47,39 @@ function EducationLevelList() {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!nameAr || !nameEn) {
+      console.log("Please fill in all required fields");
+      return;
+    }
+
+    var body = {
+      nameAr,
+      nameEn,
+    };
+
+    const submitCreate = async () => {
+      let aux = { ...body };
+      try {
+        await dispatch(createEducationLevel(aux));
+        showSuccessToast(" Education Level created successfully");
+        setAddLevelModalOpen(false);
+        setNameAr("");
+        setNameEn("");
+        dispatch(fetchEducationLevels());
+      } catch (error) {
+        console.log(error);
+        showErrorToast(error.message);
+      }
+    };
+    submitCreate();
+  };
+
   useEffect(() => {
     dispatch(fetchEducationLevels());
   }, [dispatch]);
-  console.log(educationLevelStore, "educationLevelStore");
 
   useEffect(() => {
     if (educationLevelStore?.educationLevels?.items) {
@@ -60,7 +102,6 @@ function EducationLevelList() {
       width: 155,
       editable: false,
     },
-
     { field: "nameEn", headerName: "nameEn", width: 155, editable: false },
     {
       field: "createdAt",
@@ -68,7 +109,6 @@ function EducationLevelList() {
       width: 155,
       editable: false,
     },
-
     {
       field: "actions",
       type: "actions",
@@ -97,6 +137,7 @@ function EducationLevelList() {
       },
     },
   ];
+
   return (
     <div>
       <div>
@@ -105,11 +146,14 @@ function EducationLevelList() {
             Education Level List
           </h2>
           <hr />
-          <AddButton
-            title={"Add level"}
-            mb={20}
-            onClick={() => navigate("addLevel")}
-          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setAddLevelModalOpen(true)} // Open the Add Level Modal
+            style={{ marginBottom: 20 }}
+          >
+            Add level
+          </Button>
           <Box sx={{ height: 400, width: "100%" }}>
             <DataGrid
               rows={rows}
@@ -132,6 +176,54 @@ function EducationLevelList() {
             toggleShow={toggleShow}
             confirm={() => handleDeleteeducationLevelClick()}
           />
+          {/* Add Level Modal */}
+          <Modal
+            open={isAddLevelModalOpen}
+            onClose={() => setAddLevelModalOpen(false)} // Close the Add Level Modal
+          >
+            <div className="w-100 d-flex justify-content-center align-items-center flex-column my-3">
+              <form className="checkout-form" onSubmit={handleSubmit}>
+                <div className="d-flex flex-wrap">
+                  <div className=" m-3">
+                    <Grid item xs={12}>
+                      <Typography variant="h4" align="center" gutterBottom>
+                        Create education level
+                      </Typography>
+                    </Grid>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          label="nameEn"
+                          variant="outlined"
+                          fullWidth
+                          value={nameEn}
+                          onChange={(e) => setNameEn(e.target.value)}
+                          required
+                          margin="normal"
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          label="nameAr"
+                          variant="outlined"
+                          fullWidth
+                          value={nameAr}
+                          onChange={(e) => setNameAr(e.target.value)}
+                          required
+                          margin="normal"
+                        />
+                      </Grid>
+                      <div className="w-100 d-flex justify-content-center">
+                        <Button type="submit" variant="contained" color="primary">
+                          Save
+                        </Button>
+                      </div>
+                    </Grid>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
